916eba7f90315f2619c1b70b25a87222f335bca2
Maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-03-08 04:10:01 +0200

    add new project & new skill

--- script/script.js
+++ script/script.js
@@ -7,12 +7,12 @@ const hiddenCatBtns = document.getElementById('hidden-categories-filter');
 const filterBtn = document.getElementById('filter-btn');
 const allcatbtn = document.getElementById('all-category');
 const flutterCatBtn = document.getElementById('flutter-category-btn');
-const webAppCatBtn = document.getElementById('webapp-category-btn');
+const vanillaJsCatBtn = document.getElementById('vanillaJs-category-btn');
 const webDesignCatBtn = document.getElementById('webdesigns-category-btn');
 const reactAppCatBtn = document.getElementById('reactapp-category-btn');
 const allcatbtnLg = document.getElementById('all-category-lg');
 const flutterCatBtnLg = document.getElementById('flutter-category-btn-lg');
-const webAppCatBtnLg = document.getElementById('webapp-category-btn-lg');
+const vanillaJsCatBtnLg = document.getElementById('vanillaJs-category-btn-lg');
 const webDesignCatBtnLg = document.getElementById('webdesigns-category-btn-lg');
 const reactAppCatBtnLg = document.getElementById('reactapp-category-btn-lg');
 const militaryServiceStatus = document.getElementById('militaryS_status');
@@ -26,7 +26,7 @@ const imgToolsSrc = 'Assets/images/skills/';
 const portfolioCategories = {
     all: 'All Projects',
     flutter: 'Flutter App',
-    webApp: 'Web App',
+    vanillaJs: 'Vanilla JS',
     reactApp:'React App',
     webDesign: 'Web Design'
 }
@@ -41,6 +41,7 @@ const toolsUsed = {
     dart: 'Dart',
     firebase: 'Firebase',
     react: 'React',
+    redux: 'Redux',
     ts:'TypeScript'
 }
 
@@ -67,6 +68,8 @@ const toolToImgSrc = function (tool) {
         return imgToolsSrc + 'typescript.png';
     else if (tool === toolsUsed.react)
         return imgToolsSrc + 'physics.png';
+    else if (tool === toolsUsed.redux)
+        return imgToolsSrc + 'redux.svg';
     else if (tool === toolsUsed.flutter)
         return imgToolsSrc + 'flutter.png';
     else if (tool === toolsUsed.dart)
@@ -74,7 +77,7 @@ const toolToImgSrc = function (tool) {
     else if (tool === toolsUsed.firebase)
         return imgToolsSrc + 'firebase.png';
     else if (tool === toolsUsed.tailwindCSS)
-        return imgToolsSrc + 'tailwindcss.svg';
+        return imgToolsSrc + 'tailwindcss.png';
 }
 
 const createCardUI = function (projctImgName, projectTitle, projectDescription, usedTools, githubURL, projectURL) {
@@ -175,15 +178,20 @@ addProject(portfolioCategories.flutter, `pocketMechanic.png`, 'Pocket Mechanic',
     "A cross-platform mobile application where the user can choose between a variety of vehicle maintenance services. such as: Requesting a mechanic to do a checkup and perform their service on spot, viewing the nearest available mechanic and book a reservation date with, or the nearest available tow-truck driver around the user's location. In addition to,a section where the user can purchase spare-parts to their vehicle."
     , [toolsUsed.flutter, toolsUsed.dart, toolsUsed.firebase], 'https://github.com/maghrabyy/PocketMechnicApp');
 
-addProject(portfolioCategories.webApp, `maghUniverse.png`, 'Maghh Universe',
+addProject(portfolioCategories.vanillaJs, `maghUniverse.png`, 'Maghh Universe',
     "An object oriented web application where you can choose to be hero, villian, or civil and you can add more characters of these types. The hero's role is to protect and heal the civil from the villian's attack and to attack the villain. while the villain can choose to attack the hero or the civil."
     , [toolsUsed.html, toolsUsed.css, toolsUsed.bootstrap, toolsUsed.js], 'https://github.com/maghrabyy/maghhUniverseWeb',
     'https://maghrabyy.github.io/maghhUniverseWeb/');
 
-addProject(portfolioCategories.webApp, `CharacterCreator.png`, 'Character Creator',
+addProject(portfolioCategories.vanillaJs, `CharacterCreator.png`, 'Character Creator',
     "A simple web application where you can input the required data to create a character with a random avatar image. Including the search feature where you can search for any characters by inputting any information needed."
     , [toolsUsed.html, toolsUsed.css, toolsUsed.bootstrap, toolsUsed.js], 'https://github.com/maghrabyy/characterCreator',
     'https://maghrabyy.github.io/characterCreator/');
+
+addProject(portfolioCategories.vanillaJs, `basicEcommerce.png`, 'Basic Ecommerce',
+    "A basic ecommerce website developed using Vanilla JS and bootstrap with a shopping cart and categorized products ecommerce features."
+    , [toolsUsed.html, toolsUsed.css, toolsUsed.bootstrap, toolsUsed.js], 'https://github.com/maghrabyy/Basic_Ecommerce',
+    'https://maghrabyy.github.io/Basic_Ecommerce/');
 
 addProject(portfolioCategories.webDesign, `designOne.png`, 'Web Design #1',
     "The first responsive bootstrap design with landing page."
@@ -244,10 +252,10 @@ flutterCatBtn.addEventListener('click', () => {
     renderUI();
 })
 
-webAppCatBtn.addEventListener('click', () => {
-    chosenCategory = portfolioCategories.webApp;
+vanillaJsCatBtn.addEventListener('click', () => {
+    chosenCategory = portfolioCategories.vanillaJs;
     document.querySelectorAll('.hiddenCategories li').forEach(node => node.classList.remove('active'));
-    webAppCatBtn.classList.add('active');
+    vanillaJsCatBtn.classList.add('active');
     renderUI();
 })
 
@@ -279,10 +287,10 @@ flutterCatBtnLg.addEventListener('click', () => {
     renderUI();
 })
 
-webAppCatBtnLg.addEventListener('click', () => {
-    chosenCategory = portfolioCategories.webApp;
+vanillaJsCatBtnLg.addEventListener('click', () => {
+    chosenCategory = portfolioCategories.vanillaJs;
     document.querySelectorAll('.shownCategories li').forEach(node => node.classList.remove('active'));
-    webAppCatBtnLg.classList.add('active');
+    vanillaJsCatBtnLg.classList.add('active');
     renderUI();
 })
 
